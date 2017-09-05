3d4a462b36283192b097ee1c3adb1a87f281dc04
Josh Lory <josh.lory@code.org>
2017-09-05 13:58:08 -0700

    Remove unused `whilePathAhead` API
    This API is referenced by Adventurer, but never called: https://github.com/code-dot-org/code-dot-org/search?q=whilePathAhead.

--- src/js/game/API/CodeOrgAPI.js
+++ src/js/game/API/CodeOrgAPI.js
@@ -1,7 +1,6 @@
 const PlaceBlockCommand = require("../CommandQueue/PlaceBlockCommand.js");
 const PlaceInFrontCommand = require("../CommandQueue/PlaceInFrontCommand.js");
 const MoveForwardCommand = require("../CommandQueue/MoveForwardCommand.js");
-const WhileCommand = require("../CommandQueue/WhileCommand.js");
 const IfBlockAheadCommand = require("../CommandQueue/IfBlockAheadCommand.js");
 const CallbackCommand = require("../CommandQueue/CallbackCommand.js");
 const RepeatCommand = require("../CommandQueue/RepeatCommand.js");
@@ -166,10 +165,6 @@ module.exports.get = function (controller) {
 
     tillSoil: function (highlightCallback, targetEntity) {
       controller.addCommand(new PlaceInFrontCommand(controller, highlightCallback, 'watering', targetEntity));
-    },
-
-    whilePathAhead: function (highlightCallback, blockType, targetEntity, codeBlock) {
-      controller.addCommand(new WhileCommand(controller, highlightCallback, blockType, targetEntity, codeBlock), targetEntity);
     },
 
     ifBlockAhead: function (highlightCallback, blockType, targetEntity, codeBlock) {
