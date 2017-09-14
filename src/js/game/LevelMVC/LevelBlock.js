a9181b832f34c188de20c81f56d283fd016912dc
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-09-14 14:43:34 -0700

    move all the miniblock frame inference into LevelBlock

--- src/js/game/LevelMVC/LevelBlock.js
+++ src/js/game/LevelMVC/LevelBlock.js
@@ -146,8 +146,11 @@ module.exports = class LevelBlock {
     }
   }
 
+  /**
+   * @return {boolean}
+   */
   getIsMiniblock() {
-    return this.blockType.endsWith("Miniblock")
+    return LevelBlock.isMiniblock(this.blockType);
   }
 
   getIsTree() {
@@ -164,5 +167,95 @@ module.exports = class LevelBlock {
 
   getIsEmptyOrEntity() {
     return this.isEmpty || this.isEntity;
+  }
+
+  /**
+   * Does the given block type represent a miniblock?
+   *
+   * @param {String} blockType
+   * @return {boolean}
+   */
+  static isMiniblock(blockType) {
+    return blockType.endsWith("Miniblock")
+  }
+
+  /**
+   * For any given block type, get the appropriate mini block frame (as defined
+   * in LevelView.miniblocks) if it exists.
+   *
+   * For miniblock block types, this should be the miniblock itself, so this
+   * means simply removing the "Miniblock" identifier, so a "diamondMiniblock"
+   * block will produce a "diamond" frame.
+   *
+   * For regular block types, this should be the miniblock produced when
+   * destroying the block type, so a "oreDiamond" block will produce a "diamond"
+   * frame
+   *
+   * @param {String} blockType
+   * @return {String} frame identifier
+   */
+  static getMiniblockFrame(blockType) {
+    // We don't have rails miniblock assets yet.
+    if (blockType.startsWith("rails")) {
+      return;
+    }
+
+    // We use the same miniblock for -all- restoneWire
+    if (blockType.substring(0,12) === "redstoneWire") {
+      return "redstoneDust";
+    }
+
+    // Miniblock block types are suffixed with the string "Miniblock"
+    if (LevelBlock.isMiniblock(blockType)) {
+      return blockType.replace("Miniblock", "");
+    }
+
+    // For everything else, simply map the block type to the desired miniblock
+    let frame = blockType;
+
+    switch (frame) {
+      case "treeAcacia":
+      case "treeBirch":
+      case "treeJungle":
+      case "treeOak":
+      case "treeSpruce":
+        frame = "log" + frame.substring(4);
+        break;
+      case "stone":
+        frame = "cobblestone";
+        break;
+      case "oreCoal":
+        frame = "coal";
+        break;
+      case "oreDiamond":
+        frame = "diamond";
+        break;
+      case "oreIron":
+        frame = "ingotIron";
+        break;
+      case "oreLapis":
+        frame = "lapisLazuli";
+        break;
+      case "oreGold":
+        frame = "ingotGold";
+        break;
+      case "oreEmerald":
+        frame = "emerald";
+        break;
+      case "oreRedstone":
+        frame = "redstoneDust";
+        break;
+      case "grass":
+        frame = "dirt";
+        break;
+      case "wool_orange":
+        frame = "wool";
+        break;
+      case "tnt":
+        frame = "gunPowder";
+        break;
+    }
+
+    return frame;
   }
 };
