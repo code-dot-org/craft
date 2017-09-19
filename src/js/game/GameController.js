77c77125d92c198e3cfa0eb9fcf25b6fb975fa06
William Harmon <v-wiharm@microsoft.com>
2017-09-19 15:44:34 -0700

    added a moveBackward command that will be needed for this year's functionality

--- src/js/game/GameController.js
+++ src/js/game/GameController.js
@@ -784,6 +784,32 @@ class GameController {
     }
   }
 
+  moveBackward(commandQueueItem) {
+    let target = commandQueueItem.target;
+    if (!this.isType(target)) {
+      // apply to all entities
+      if (target === undefined) {
+        let entities = this.levelEntity.entityMap;
+        for (var value of entities) {
+          let entity = value[1];
+          let callbackCommand = new CallbackCommand(this, () => { }, () => { this.moveForward(callbackCommand); }, entity.identifier);
+          entity.addCommand(callbackCommand, commandQueueItem.repeat);
+        }
+        commandQueueItem.succeeded();
+      } else {
+        let entity = this.getEntity(target);
+        entity.moveForward(commandQueueItem);
+      }
+    } else {
+      let entities = this.getEntities(target);
+      for (let i = 0; i < entities.length; i++) {
+        let callbackCommand = new CallbackCommand(this, () => { }, () => { this.moveForward(callbackCommand); }, entities[i].identifier);
+        entities[i].addCommand(callbackCommand, commandQueueItem.repeat);
+      }
+      commandQueueItem.succeeded();
+    }
+  }
+
   moveDirection(commandQueueItem, direction) {
     let player = this.getEntity(commandQueueItem.target);
     let isMovingOffOf = this.levelModel.isEntityOnBlocktype(commandQueueItem.target, "pressurePlateDown");
