cae791b8e6cb9c6edd228f852d6237ea6e44f1a2
William Harmon <v-wiharm@microsoft.com>
2017-10-02 16:24:16 -0700

    updated a unit test because rails now have mini block! Hurray

--- test/unit/LevelBlockTest.js
+++ test/unit/LevelBlockTest.js
@@ -27,7 +27,7 @@ test('getMiniblockFrame', t => {
   });
 
   // Rails give us nothing
-  t.equal(LevelBlock.getMiniblockFrame("rails"), undefined);
+  t.equal(LevelBlock.getMiniblockFrame("rails"), "railNormal");
 
   // Generally, the in-world blocks, the miniblock-specific blocks, and the
   // miniblock frame itself should all resolve to just the miniblock frame
