18642e4a27359941db6161f53d124c68aa3a6bfa
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-10-24 13:32:32 -0700

    add a simple Position class; right now just to collect static helper methods

---
+++ src/js/game/LevelMVC/Position.js
@@ -0,0 +1,24 @@
+const FacingDirection = require("./FacingDirection");
+
+const directions = [
+  FacingDirection.North,
+  FacingDirection.East,
+  FacingDirection.South,
+  FacingDirection.West
+];
+
+module.exports = class Position {
+  static add(left, right) {
+    return [left[0] + right[0], left[1] + right[1]];
+  }
+
+  static equals(left, right) {
+    return left[0] === right[0] && left[1] === right[1];
+  }
+
+  static isAdjacent(left, right) {
+    return directions
+        .map(FacingDirection.directionToOffset)
+        .any(offset => Position.equals(Position.add(left, offset), right));
+  }
+};
