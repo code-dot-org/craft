b3bc0a2b27d1f1229254662cfc48963043fef975
William Harmon <v-wiharm@microsoft.com>
2017-11-20 11:13:09 -0800

    PR feedback

--- src/js/game/LevelMVC/LevelView.js
+++ src/js/game/LevelMVC/LevelView.js
@@ -771,13 +771,9 @@ module.exports = class LevelView {
   * Handling the first case of walking onto a track while not currently on one
   */
   isFirstTimeOnRails(currPos, nextPos) {
-    if (!this.controller.levelModel.actionPlane.inBounds(currPos) ||
-        !this.controller.levelModel.actionPlane.inBounds(nextPos)) {
-      return false;
-    }
     let nextBlock = this.controller.levelModel.actionPlane.getBlockAt(nextPos);
     let currBlock = this.controller.levelModel.actionPlane.getBlockAt(currPos);
-    if (!currBlock.isRail && nextBlock.isRail) {
+    if ((nextBlock && currBlock) && (!currBlock.isRail && nextBlock.isRail)) {
       return true;
     }
     return false;
