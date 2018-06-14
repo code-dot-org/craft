6a79c8246a7c08b51cddea6991e1d1cbe17f240f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-06-14 15:20:10 -0700

    remove manual phaser test dependency, add dependency from npm

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ module.exports = config => {
     basePath: '',
     frameworks: ['tap', 'browserify'],
     files: [
-      {pattern: 'test/helpers/phaser.js'},
+      {pattern: 'node_modules/phaser/build/phaser.js'},
       {pattern: 'test/integration/*.js'},
       {pattern: 'src/assets/**/*.*', watched: false, included: false},
     ],
