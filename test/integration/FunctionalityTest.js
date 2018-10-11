bb8d5eecbd0cc734fe657de204d99ef16ffeb584
Brad Buchanan <bradley.c.buchanan@gmail.com>
2018-10-11 15:03:16 -0700

    New async pattern for FunctionalityTest

--- test/integration/FunctionalityTest.js
+++ test/integration/FunctionalityTest.js
@@ -3,28 +3,25 @@ const attempt = require("../helpers/RunLevel.js");
 const Position = require("../../src/js/game/LevelMVC/Position");
 
 test('Pistons: Entity Obstruction 1', t => {
-  attempt('functionality01', api => new Promise(resolve => {
+  attempt('functionality01', async (api, levelModel) => {
     // Power the piston
     api.placeInFront(null, 'railsRedstoneTorch', 'Player');
     api.turnLeft(null, 'Player');
     api.moveForward(null, 'Player');
 
     // Check the solutions. No piston arm should exist because Agent conflicts with the index
-    api.startAttempt((success, levelModel) => {
-      t.deepEqual(levelModel.actionPlane._data[21].blockType, "");
+    const success = await api.startAttempt();
+    t.deepEqual(levelModel.actionPlane._data[21].blockType, "");
 
-      t.true(Position.equals(levelModel.player.position, new Position(4, 3)));
-      t.true(Position.equals(levelModel.agent.position, new Position(1, 2)));
-      t.assert(success);
-      t.end();
-
-      resolve();
-    });
-  }), 1);
+    t.true(Position.equals(levelModel.player.position, new Position(4, 3)));
+    t.true(Position.equals(levelModel.agent.position, new Position(1, 2)));
+    t.assert(success);
+    t.end();
+  }, 1);
 });
 
 test('Pistons: Entity Obstruction 2', t => {
-  attempt('functionality01', api => new Promise(resolve => {
+  attempt('functionality01', async (api, levelModel) => {
     // Power the piston
     api.placeInFront(null, 'railsRedstoneTorch', 'Player');
     api.turnLeft(null, 'Player');
@@ -41,21 +38,18 @@ test('Pistons: Entity Obstruction 2', t => {
     }, 2000);
 
     // Check the solutions
-    api.startAttempt((success, levelModel) => {
-      t.deepEqual(levelModel.actionPlane._data[21].blockType, "pistonArmLeft");
+    const success = await api.startAttempt();
+    t.deepEqual(levelModel.actionPlane._data[21].blockType, "pistonArmLeft");
 
-      t.true(Position.equals(levelModel.player.position, new Position(4, 3)));
-      t.true(Position.equals(levelModel.agent.position, new Position(1, 3)));
-      t.assert(success);
-      t.end();
-
-      resolve();
-    });
-  }), 1);
+    t.true(Position.equals(levelModel.player.position, new Position(4, 3)));
+    t.true(Position.equals(levelModel.agent.position, new Position(1, 3)));
+    t.assert(success);
+    t.end();
+  }, 1);
 });
 
 test('Rails: Moving On to Ride', t => {
-  attempt('functionality02', api => new Promise(resolve => {
+  attempt('functionality02', async (api, levelModel) => {
 
     // Have the player walk onto the rails and ride them to the end of the track.
     api.moveDirection(null, 'Player', 1);
@@ -67,35 +61,27 @@ test('Rails: Moving On to Ride', t => {
       }
     }, 10000);
 
-
     // Check the solutions
-    api.startAttempt((success, levelModel) => {
-      t.true(Position.equals(levelModel.player.position, new Position(9, 9)));
-      t.assert(success);
-      t.end();
-
-      resolve();
-    });
-  }));
+    const success = await api.startAttempt();
+    t.true(Position.equals(levelModel.player.position, new Position(9, 9)));
+    t.assert(success);
+    t.end();
+  });
 });
 
 test('Pressure Plate: Moving On to Rail', t => {
-  attempt('functionality03', api => new Promise(resolve => {
+  attempt('functionality03', async (api, levelModel) => {
 
     // Have the player walk onto the rails and ride them to the end of the track.
     api.moveDirection(null, 'Player', 2);
     api.moveDirection(null, 'Player', 1);
 
-
     // Check the solutions
-    api.startAttempt((success, levelModel) => {
-      t.true(Position.equals(levelModel.player.position, new Position(6, 2)));
-      t.notOk(levelModel.actionPlane._data[0].isPowered);
-      t.assert(success);
-      t.end();
-
-      resolve();
-    });
-  }));
+    const success = await api.startAttempt();
+    t.true(Position.equals(levelModel.player.position, new Position(6, 2)));
+    t.notOk(levelModel.actionPlane._data[0].isPowered);
+    t.assert(success);
+    t.end();
+  });
 });
 
